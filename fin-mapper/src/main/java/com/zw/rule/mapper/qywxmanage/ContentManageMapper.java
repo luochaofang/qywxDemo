99fa940f0f5eb849a72bb34fe677d1fe6af3d470
luochaofang <rbt021@163.com>
2018-07-30 17:58:31 +0800

    添加基础文件

--- fin-mapper/src/main/java/com/zw/rule/mapper/qywxmanage/ContentManageMapper.java
+++ fin-mapper/src/main/java/com/zw/rule/mapper/qywxmanage/ContentManageMapper.java
@@ -2,6 +2,9 @@ package com.zw.rule.mapper.qywxmanage;
 
 import com.zw.rule.qywxmanage.Content;
 
+/**
+ * 推广内容mapper
+ */
 public interface ContentManageMapper {
 
     int insertSelective(Content record);
--- fin-mapper/src/main/java/com/zw/rule/mapper/qywxmanage/ContentSeeManageMapper.java
+++ fin-mapper/src/main/java/com/zw/rule/mapper/qywxmanage/ContentSeeManageMapper.java
@@ -3,6 +3,9 @@ package com.zw.rule.mapper.qywxmanage;
 
 import com.zw.rule.qywxmanage.ContentSee;
 
+/**
+ * 推广内容已查看mapper
+ */
 public interface ContentSeeManageMapper {
 
     int insertSelective(ContentSee record);
